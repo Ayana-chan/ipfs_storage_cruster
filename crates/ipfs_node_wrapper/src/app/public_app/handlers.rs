1493d594cf15a8ba9cdad3d6ef894e6a7d75faee
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-07 23:18:15 +0800

    handler type correct

--- crates/ipfs_node_wrapper/src/app/public_app/handlers.rs
+++ crates/ipfs_node_wrapper/src/app/public_app/handlers.rs
@@ -1,14 +1,46 @@
 use axum::extract::{Query, State};
-use tracing::info;
+use axum::http::{HeaderMap, HeaderValue};
+use axum::response::IntoResponse;
+use tracing::{info, trace};
 use crate::app::public_app::PublicAppState;
 use crate::models;
 use crate::ipfs_client;
 
+#[axum_macros::debug_handler]
 #[tracing::instrument(skip_all)]
-pub async fn get_file(State(state): State<PublicAppState>, Query(query): Query<models::GetFileArgs>) -> Result<String, String> {
+pub async fn get_file(State(state): State<PublicAppState>, Query(query): Query<models::GetFileArgs>) -> Result<(HeaderMap, impl IntoResponse), String> {
     info!("Get File");
-    let content = ipfs_client::ipfs_get_file(&query.cid, &state.app_state.ipfs_node_metadata).await?;
-    Ok(content)
+    let ipfs_res = ipfs_client::ipfs_get_file(&query.cid, &state.app_state.ipfs_node_metadata).await;
+    if let Err(e) = ipfs_res {
+        return Err(e); //TODO 定制error
+    }
+    let ipfs_res = ipfs_res.unwrap();
+
+    // construct header
+    let ipfs_res_header = ipfs_res.headers();
+    trace!("header: {:#?}", ipfs_res_header);
+    let mut header = HeaderMap::new();
+    let header_value;
+    header_value = ipfs_res_header
+        .get("content-type")
+        .map(|v|
+            v.to_str().unwrap_or_default()
+        );
+    if let Some(v) = header_value {
+        let hv = HeaderValue::from_str(v);
+        if let Ok(hv) = hv {
+            header.insert("content-type", hv);
+        }
+    }
+
+    let content = ipfs_res.text().await;
+    if let Err(e) = content {
+        return Err(e.to_string());
+    }
+    let content = content.unwrap();
+    trace!("text: {:?}", content);
+
+    Ok((header.to_owned(), content))
 }
 
 
--- crates/ipfs_node_wrapper/src/ipfs_client.rs
+++ crates/ipfs_node_wrapper/src/ipfs_client.rs
@@ -1,4 +1,4 @@
-use tracing::trace;
+use reqwest::Response;
 
 #[derive(Default, Clone, Debug)]
 pub struct IpfsNodeMetadata {
@@ -6,30 +6,18 @@ pub struct IpfsNodeMetadata {
     pub rpc_address: String,
 }
 
+// TODO 建立struct来访问ipfs，然后把结构体存在state中
+
 #[tracing::instrument(skip_all)]
-pub async fn ipfs_get_file(cid: &str, ipfs_node_metadata: &parking_lot::RwLock<IpfsNodeMetadata>) -> Result<String, String> {
+pub async fn ipfs_get_file(cid: &str, ipfs_node_metadata: &parking_lot::RwLock<IpfsNodeMetadata>) -> Result<Response, String> {
     let url = "http://".to_string() +
         &ipfs_node_metadata.read().gateway_address +
         "/ipfs/" + cid;
     let res = reqwest::Client::new()
         .get(url)
         .send()
-        .await
-        .map(|v| v.error_for_status());
-    if let Err(e) = res {
-        return Err(e.to_string());
-    }
-    let res = res.unwrap();
-    if let Err(e) = res {
-        return Err(e.to_string());
-    }
-    let res = res.unwrap();
+        .await.map_err(|e| e.to_string())?
+        .error_for_status().map_err(|e| e.to_string())?;
 
-    let content = res.text().await;
-    if let Err(e) = content {
-        return Err(e.to_string());
-    }
-    let content = content.unwrap();
-    trace!("text: {:?}", content);
-    Ok(content)
+    Ok(res)
 }
