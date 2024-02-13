0d829fbc49663c0153b15dd9afe562ac7d4c9472
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-13 18:40:48 +0800

    async_tasks_recorder crate

---
+++ crates/async_tasks_recorder/src/lib.rs
@@ -0,0 +1,97 @@
+//! `failed` <---> `working` ----> `success`
+
+use std::future::Future;
+use std::sync::Arc;
+
+/// Thread-safe.
+#[derive(Default, Debug)]
+pub struct TaskManager {
+    /// hot
+    pub working_tasks: scc::HashSet<String>,
+    pub success_tasks: scc::HashSet<String>,
+    pub failed_tasks: scc::HashSet<String>,
+}
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+pub enum TaskStatus {
+    /// running or pending
+    Working,
+    Pinned,
+    Failed,
+}
+
+/// Safe to clone.
+#[derive(Default, Debug, Clone)]
+pub struct AsyncTasksRecoder {
+    task_manager: Arc<TaskManager>,
+}
+
+impl AsyncTasksRecoder {
+    pub fn new() -> Self {
+        AsyncTasksRecoder {
+            task_manager: TaskManager::default().into()
+        }
+    }
+
+    /// success_tasks -> failed_tasks -> working_tasks
+    /// If not found in all tasks, be `Failed`.
+    pub async fn get_task_status(&self, cid: &str) -> TaskStatus {
+        if self.task_manager.success_tasks.contains_async(cid).await {
+            return TaskStatus::Pinned;
+        }
+
+        if self.task_manager.failed_tasks.contains_async(cid).await {
+            return TaskStatus::Failed;
+        }
+
+        if self.task_manager.working_tasks.contains_async(cid).await {
+            return TaskStatus::Working;
+        }
+
+        return TaskStatus::Failed;
+    }
+
+    /// Launch task.
+    pub async fn launch<Fut, R, E>(&self, task_id: &str, add_pin_task: Fut)
+        where Fut: Future<Output=Result<R, E>> + Send + 'static,
+              R: Send,
+              E: Send {
+        // check success -> insert working -> remove failed -> work -> insert success -> remove working
+        // modify status
+        if self.task_manager.success_tasks.contains_async(task_id).await {
+            // succeeded
+            return;
+        }
+        let res = self.task_manager.working_tasks.insert_async(task_id.to_string()).await;
+        if res.is_err() {
+            // on working
+            return;
+        }
+        // remove from `failed_tasks` if contained
+        self.task_manager.failed_tasks.remove_async(task_id).await;
+
+        // adjust args
+        let task_manager = self.task_manager.clone();
+        let cid = task_id.to_string();
+
+        // start
+        let _task = tokio::spawn(async move {
+            let add_pin_res = add_pin_task.await;
+            // TODO 优化这里的string clone
+            if add_pin_res.is_ok() {
+                let _ = task_manager.success_tasks.insert_async(cid.clone()).await;
+                task_manager.working_tasks.remove_async(&cid).await;
+            } else {
+                // more contention or more memory copy?
+                task_manager.working_tasks.remove_async(&cid).await;
+                let _ = task_manager.failed_tasks.insert_async(cid).await;
+            }
+        });
+    }
+
+    /// Get a cloned `Arc` of `task_manager`. Then you can do anything you want.
+    pub fn get_task_manager(&self) -> Arc<TaskManager> {
+        self.task_manager.clone()
+    }
+}
+
---
+++ crates/async_tasks_recorder/tests/async_tasks_recorder_tests.rs
@@ -0,0 +1,101 @@
+use async_tasks_recorder::*;
+use std::future::Future;
+
+static DEFAULT_CHECK_INTERVAL_MS: u64 = 5;
+static DEFAULT_CHECK_TIMEOUT_MS: u128 = 1000;
+
+enum RuntimeType {
+    CurrentThread,
+    CustomThread(usize),
+    MultiThread,
+}
+
+#[test]
+fn test_add_pin_manager_basic() {
+    do_async_test(
+        RuntimeType::MultiThread,
+        test_add_pin_manager_basic_core(),
+    );
+}
+
+// #[test]
+// fn test_add_pin_manager_serial() {}
+//
+// #[test]
+// fn test_add_pin_manager_random() {}
+
+// core functions ---------------------------------------------------------------
+
+async fn test_add_pin_manager_basic_core() {
+    let manager = AsyncTasksRecoder::new();
+    manager.launch("t1", empty_task()).await;
+    check_success(&manager, "t1", None, None).await;
+}
+
+// tools ------------------------------------------------------------------------
+
+fn do_async_test<Fut>(runtime_type: RuntimeType, test_future: Fut)
+    where Fut: Future {
+    let runtime;
+    match runtime_type {
+        RuntimeType::CurrentThread => {
+            runtime = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build().unwrap();
+        }
+        RuntimeType::CustomThread(thread_num) => {
+            runtime = tokio::runtime::Builder::new_multi_thread()
+                .worker_threads(thread_num)
+                .enable_all()
+                .build().unwrap();
+        }
+        RuntimeType::MultiThread => {
+            runtime = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build().unwrap();
+        }
+    }
+
+    runtime.block_on(test_future);
+}
+
+async fn check_success_once(manager: &AsyncTasksRecoder, cid: &str) -> bool {
+    manager.get_task_status(cid).await == TaskStatus::Pinned
+}
+
+// TODO failed re-pin机制
+/// Err when timeout
+async fn check_success(manager: &AsyncTasksRecoder, cid: &str, interval_ms: Option<u64>, timeout_ms: Option<u128>) {
+    let interval_ms = interval_ms.unwrap_or(DEFAULT_CHECK_INTERVAL_MS.clone());
+    let timeout_ms = timeout_ms.unwrap_or(DEFAULT_CHECK_TIMEOUT_MS.clone());
+
+    let start_time = std::time::Instant::now();
+
+    loop {
+        // timeout
+        if start_time.elapsed().as_millis() >= timeout_ms {
+            panic!("Timeout before success. cid: {:?}", cid);
+        }
+
+        if check_success_once(manager, cid).await {
+            return;
+        }
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(interval_ms.clone())).await;
+    }
+}
+
+// TODO 定制延迟；定制概率（至少不能是50%）
+/// A task always return ok.
+async fn empty_task() -> Result<(), ()> {
+    Ok(())
+}
+
+/// A task possibly return err.
+async fn empty_random_task() -> Result<(), ()> {
+    if fastrand::bool() {
+        Ok(())
+    } else {
+        Err(())
+    }
+}
