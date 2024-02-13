f31667a07fc6a05642ddfda007ea15b0f3f8ab1b
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-13 21:32:50 +0800

    proof

--- crates/async_tasks_recorder/src/lib.rs
+++ crates/async_tasks_recorder/src/lib.rs
@@ -26,6 +26,45 @@ pub struct AsyncTasksRecoder {
     task_manager: Arc<TaskManager>,
 }
 
+/// # Proof
+///
+/// ## P01
+/// **A task (or tasks with the same `task_id`) wouldn't be executed again after one success.** \
+/// `working_tasks` play the role of lock,
+/// which only allow tasks with the same `task_id` to execute remaining codes before `remove` from `working_tasks`.
+/// And before `remove` from `working_tasks`, the succeeded `task_id` has been in `success_tasks`.
+///
+/// When a task fail, it wouldn't break anything. Failure just means the task could be launched again,
+/// so if the proposition is true, there is almost nothing to worry about.
+///
+/// ```not_rust
+/// let success_tasks = atomic(0);
+/// let count = atomic(0);
+/// loop_multi_thread {
+///     working_tasks.lock();
+///     if success_tasks.get() == 1 exit();
+///     count.add(1);
+///     success_tasks.set(1);
+///     working_tasks.unlock();
+/// }
+/// assert_eq!(count.get(), 1);
+/// ```
+///
+/// ```not_rust
+/// let success_tasks = atomic(0);
+/// let count = atomic(0);
+/// loop_multi_thread {
+///     working_tasks.lock();
+///     if success_tasks.get() == 1 exit();
+///     success_tasks.set(1);
+///     count.add(1);
+///     working_tasks.unlock();
+/// }
+/// assert_eq!(count.get(), 1);
+/// ```
+///
+/// ## P02
+/// ** **`failed task` is only for optimizing the failure judgment.
 impl AsyncTasksRecoder {
     pub fn new() -> Self {
         AsyncTasksRecoder {
@@ -39,15 +78,16 @@ impl AsyncTasksRecoder {
         where Fut: Future<Output=Result<R, E>> + Send + 'static,
               R: Send,
               E: Send {
-        // check success -> insert working -> remove failed -> work -> insert success -> remove working
-        // modify status
-        if self.task_manager.success_tasks.contains_async(task_id).await {
-            // succeeded
-            return;
-        }
+        // insert working -> check success -> remove failed -> work -> insert success -> remove working
+        // `working_tasks` play the role of lock
         let res = self.task_manager.working_tasks.insert_async(task_id.to_string()).await;
         if res.is_err() {
             // on working
+            return;
+        }
+        // modify status
+        if self.task_manager.success_tasks.contains_async(task_id).await {
+            // succeeded
             return;
         }
         // remove from `failed_tasks` if contained
