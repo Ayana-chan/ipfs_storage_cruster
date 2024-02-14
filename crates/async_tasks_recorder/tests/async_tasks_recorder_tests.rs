819ce41d88a35e3d08e82b8bf21ab092b486ee86
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-14 11:41:35 +0800

    check vec pass

--- crates/async_tasks_recorder/tests/async_tasks_recorder_tests.rs
+++ crates/async_tasks_recorder/tests/async_tasks_recorder_tests.rs
@@ -1,5 +1,6 @@
 use async_tasks_recorder::*;
 use std::future::Future;
+use std::sync::Arc;
 
 static DEFAULT_CHECK_INTERVAL_MS: u64 = 5;
 static DEFAULT_CHECK_TIMEOUT_MS: u128 = 1000;
@@ -30,7 +31,7 @@ fn test_once_single() {
 fn test_basic() {
     do_async_test(
         RuntimeType::CurrentThread,
-        test_basic_core(),
+        test_basic_core(5),
     );
 }
 
@@ -48,12 +49,18 @@ async fn test_once_core() {
     check_success(&manager, &id, None, None).await;
 }
 
-async fn test_basic_core() {
+async fn test_basic_core(task_num: usize) {
     let manager = AsyncTasksRecoder::new();
-    let mut task_id_generator = get_task_id_generator();
 
-    manager.launch("t1", success_task()).await;
-    check_success(&manager, "t1", None, None).await;
+    let task_id_vec = generate_task_id_vec(task_num);
+    let shuffled_map_1 = get_shuffled_index_map(task_num);
+
+    for i in 0..task_num {
+        // println!("shuffled_map_1[{}]: {}", i, shuffled_map_1[i]);
+        manager.launch(&task_id_vec[shuffled_map_1[i]], success_task()).await;
+    }
+
+    check_vec(&manager, &task_id_vec.into(), None, None).await;
 }
 
 // tools ------------------------------------------------------------------------
@@ -108,6 +115,25 @@ async fn check_success(manager: &AsyncTasksRecoder, task_id: &str, interval_ms: 
     }
 }
 
+/// Check all task_id in a vec randomly and parallelly.
+async fn check_vec(manager: &AsyncTasksRecoder, task_id_vec: &Arc<Vec<String>>, interval_ms: Option<u64>, timeout_ms: Option<u128>) {
+    let task_num = task_id_vec.len();
+    let shuffled_map = get_shuffled_index_map(task_num);
+    let mut join_set = tokio::task::JoinSet::new();
+    for i in 0..task_num {
+        let manager_backup = manager.clone();
+        let task_id_vec = task_id_vec.clone();
+        let mapped_index = shuffled_map[i];
+        let fut = async move {
+            println!("check {}", mapped_index);
+            check_success(&manager_backup, &task_id_vec[mapped_index],
+                          interval_ms.clone(), timeout_ms.clone()).await;
+        };
+        join_set.spawn(fut);
+    }
+    while let Some(_) = join_set.join_next().await {}
+}
+
 /// return a closure to generate incremental `task_id` string.
 fn get_task_id_generator() -> impl FnMut() -> String {
     let rand_str: String = std::iter::repeat_with(fastrand::alphabetic).take(6).collect();
@@ -117,6 +143,24 @@ fn get_task_id_generator() -> impl FnMut() -> String {
         count += 1;
         ans
     }
+}
+
+/// generate `task_id` vec with a new generator from `get_task_id_generator`
+fn generate_task_id_vec(task_num: usize) -> Vec<String> {
+    let mut task_id_generator = get_task_id_generator();
+    let mut task_id_vec: Vec<String> = Vec::new();
+    task_id_vec.resize(task_num, Default::default());
+    for i in 0..task_num {
+        task_id_vec[i] = task_id_generator();
+    }
+    task_id_vec
+}
+
+/// get a shuffled `vec[0, 1, 2, ...]` to map index
+fn get_shuffled_index_map(length: usize) -> Vec<usize> {
+    let mut map: Vec<usize> = (0..length).collect();
+    fastrand::shuffle(&mut map);
+    map
 }
 
 // TODO 定制延迟；定制概率（至少不能是50%）
