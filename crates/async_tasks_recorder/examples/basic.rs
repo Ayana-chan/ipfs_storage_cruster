aae3f97aad82c4a74e001992a1eb948439d55680
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-19 10:32:30 +0800

    finish example

--- crates/async_tasks_recorder/examples/basic.rs
+++ crates/async_tasks_recorder/examples/basic.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code, unused_variables)]
+
 use std::sync::Arc;
-use async_tasks_recorder::AsyncTasksRecoder;
+use async_tasks_recorder::{AsyncTasksRecoder, TaskState};
 
 struct SimulatedStream {}
 
@@ -9,6 +11,7 @@ struct UploadFileArgs {
     md5: String,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum UploadTaskState {
     Uploading,
     Failed,
@@ -27,18 +30,47 @@ fn main() {
 async fn simulate_requests() {
     println!("hello world!");
     let recorder = AsyncTasksRecoder::new();
+    let fake_md5 = "d8q793wye1u3".to_string();
+
+    println!("REQUEST: check_upload_state {}", fake_md5);
+    let result = check_upload_state(
+        recorder.clone(),
+        fake_md5.to_string()
+    ).await;
+    assert_eq!(result, UploadTaskState::Failed);
+    println!("RESPONSE: check_upload_state {}: {:?}", fake_md5, result);
+
+    println!("REQUEST: upload_file {}", fake_md5);
     upload_file(recorder.clone(),
                 UploadFileArgs {
                     stream: SimulatedStream {},
-                    md5: "d8q793wye1u3".to_string(),
+                    md5: fake_md5.clone(),
                 },
     ).await;
+
+    println!("REQUEST: check_upload_state {}", fake_md5);
+    let result = check_upload_state(
+        recorder.clone(),
+        fake_md5.to_string()
+    ).await;
+    assert_eq!(result, UploadTaskState::Uploading);
+    println!("RESPONSE: check_upload_state {}: {:?}", fake_md5, result);
+
+    println!("WAIT");
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    println!("REQUEST: check_upload_state {}", fake_md5);
+    let result = check_upload_state(
+        recorder.clone(),
+        fake_md5.to_string()
+    ).await;
+    assert_eq!(result, UploadTaskState::Success);
+    println!("RESPONSE: check_upload_state {}: {:?}", fake_md5, result);
 }
 
 // APIs -----------
 
 async fn upload_file(recorder: AsyncTasksRecoder<Arc<String>>, args: UploadFileArgs) {
-    println!("REQUEST: upload_file");
     let destination = "some place".to_string(); // decided by some algorithm
     let fut = async move {
         println!("upload_to_destination start!");
@@ -59,8 +91,15 @@ async fn upload_file(recorder: AsyncTasksRecoder<Arc<String>>, args: UploadFileA
     recorder.launch(args.md5.into(), fut).await;
 }
 
-async fn check_upload_state() -> UploadTaskState {
-    UploadTaskState::Success
+async fn check_upload_state(recorder: AsyncTasksRecoder<Arc<String>>, arg_md5: String) -> UploadTaskState {
+    let arg_md5 = arg_md5.into();
+    let res = recorder.query_task_state(&arg_md5).await;
+
+    match res {
+        TaskState::Success => UploadTaskState::Success,
+        TaskState::Failed => UploadTaskState::Failed,
+        TaskState::Working => UploadTaskState::Uploading,
+    }
 }
 
 // other functions ------------
