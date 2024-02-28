bb13552b81ff9adb0af9798062e3c7574f465e4f
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-02-28 11:08:13 +0800

    tokio version

--- crates/ipfs_node_wrapper_client/src/admin/mod.rs
+++ crates/ipfs_node_wrapper_client/src/admin/mod.rs
@@ -1,7 +1,18 @@
+mod normal;
 mod pin;
 mod traffic;
 
 pub use pin::*;
-pub use tracing::*;
+pub use traffic::*;
 
+pub struct IpfsNodeWrapperAdminClient {
+    client: reqwest::Client,
+}
 
+impl IpfsNodeWrapperAdminClient {
+    pub fn new() -> Self {
+        IpfsNodeWrapperAdminClient {
+            client: reqwest::Client::new(),
+        }
+    }
+}
