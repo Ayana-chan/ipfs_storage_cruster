d9f66597725a69eaf8c926f26fe5d6afd9e464f9
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2024-04-06 14:57:04 +0800

    doc; better trait argument

--- crates/ipfs_storage_cruster_manager/src/file_decision.rs
+++ crates/ipfs_storage_cruster_manager/src/file_decision.rs
@@ -1,9 +1,13 @@
+use sea_orm::DatabaseConnection;
+
 pub struct FileStoreDecision<M> {
+    /// Decision maker.
     pub(crate) maker: M,
 }
 
 impl<M> FileStoreDecision<M>
     where M: FileStoreDecisionMaker {
+    /// Create with a customized decision maker.
     pub fn with_decision_maker(maker: M) -> Self {
         FileStoreDecision {
             maker
@@ -11,9 +15,18 @@ impl<M> FileStoreDecision<M>
     }
 }
 
+impl<M> Default for FileStoreDecision<M> {
+    fn default() -> Self {
+        todo!()
+    }
+}
+
 pub trait FileStoreDecisionMaker {
     /// Decide which nodes to store data on.
     ///
     /// Return target `rpc_address` list.
-    fn decide_store_node(&mut self) -> impl std::future::Future<Output = Vec<String>> + Send;
+    fn decide_store_node(&mut self,
+                         db_conn: &DatabaseConnection,
+                         reqwest_client: &reqwest::Client
+    ) -> impl std::future::Future<Output = Vec<String>> + Send;
 }
